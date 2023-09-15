036dc24140340d573e39d09615333a39f49f93ff
Eugen Freund <Eugen.Freund@outlook.de>
2023-09-15 12:45:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>Time Hacker</h1>
---
+++ js/game.js
@@ -0,0 +1,12 @@
+class Game {
+    // code to be added
+    constructor() {
+
+    }
+
+    start() {
+        this.startScreen.style.display = 'none'
+        this.gameEndScreen.style.display = 'none'
+        this.gameScreen.style.display = 'block'
+    }
+}
